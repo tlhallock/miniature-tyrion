82994323d677494d8be8f6eb6a2b170eafffa3d6
thallock <dont@email.me>
2015-03-11 13:57:41 -0600

    See a screen now

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -53,7 +53,7 @@ int main(int argc, char **argv)
 
 //  PropertyFile pf {"test"};
   
-//  std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+  std::this_thread::sleep_for(std::chrono::milliseconds(10000));
 
 
 
